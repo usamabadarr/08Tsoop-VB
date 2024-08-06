599be689b33aa9566946acc488ab54ec355cb069
usamabadarr <usamabadarr@gmail.com>
2024-08-06 16:18:09 -0400

    uncommented trucks and motorbikes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import Cli from "./classes/Cli.js";
 const vehicles = [];
 
 // TODO: uncomment once trucks are implemented
-const truck = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
 
 // will use default wheels
 const car = new Car(
@@ -25,15 +25,15 @@ const car = new Car(
 
 // TODO: uncomment once motorbikes are implemented
 const motorbikeWheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
-const motorbike = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbikeWheels);
+const motorbike = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
 
 // push vehicles to array
 // TODO: uncomment once trucks are implemented
-vehicles.push(truck); 
-vehicles.push(car); 
+vehicles.push(truck1); 
+vehicles.push(car1); 
 
 // TODO: uncomment once motorbikes are implemented
-vehicles.push(motorbike);
+vehicles.push(motorbike1);
 
 // create a new instance of the Cli class
 const cli = new Cli(vehicles);
